9d88c1a3171f1b4807fe7a72ccb16b984d6c5234
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2024-08-21 15:04:53 -0600

    added reports

--- goals/urls.py
+++ goals/urls.py
@@ -5,11 +5,12 @@ from goals.views import (
     GoalMetricsApiView,
     GoalCreateApiView,
     GoalRetrieveApiView,
-
+    handleReport
 )
 
 urlpatterns = [
     path('', GoalCreateApiView.as_view(), name='goal-create'),
     path('metrics/', GoalMetricsApiView.as_view(), name='goal-metrics'),
     path('<int:pk>/', GoalRetrieveApiView.as_view(), name='goal-detail'),
+    path('metrics/report/', handleReport, name="handlereport")
 ]
--- goals/views.py
+++ goals/views.py
@@ -7,7 +7,12 @@ from goals.serializers import GoalSerializer, GoalMetricsSerializer
 from django.core import exceptions
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import get_template
+from django_xhtml2pdf.utils import pisa
+from io import BytesIO
+from django.shortcuts import HttpResponse
 
 class GoalCreateApiView(ListCreateAPIView):
     queryset = Goal.objects.all()
@@ -33,6 +38,36 @@ class GoalMetricsApiView(ListAPIView):
             goal_metric = GoalMetrics(appointments = goal_appointments.count(), monthly_goal_porcentage = goal_appointments.count()/set_goal.apponitments_goal, assistance =goal_appointments.filter(status="DONE").count())
             goal_metric_serialied = GoalMetricsSerializer(goal_metric)
             return Response(goal_metric_serialied.data)
+        except exceptions.ObjectDoesNotExist:
+            return Response(
+                {'message': 'No set goal'},
+                status=HTTP_404_NOT_FOUND
+            )
+        
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)#, link_callback=fetch_resources)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+def handleReport(request):
+    if request.method == "GET":
+        try:
+            set_goal = Goal.objects.get(start_time__lte = now().date(), end_time__gte = now().date())
+            goal_appointments = Appointment.objects.filter(goal=set_goal.pk)
+            data = {"appointments" : goal_appointments.count(), "monthly_goal_porcentage" : goal_appointments.count()/set_goal.apponitments_goal, "assistance" : goal_appointments.filter(status="DONE").count()}
+            pdf = render_to_pdf('goal/report.html',data)
+            if pdf:
+                response = HttpResponse(pdf, content_type='application/pdf')
+                filename = "report.pdf"
+                content = "inline; filename='%s'" %(filename)
+                content = "attachment; filename=%s" %(filename)
+                response['Content-Disposition'] = content
+                return response
+            return HttpResponse("Not found")
         except exceptions.ObjectDoesNotExist:
             return Response(
                 {'message': 'No set goal'},
