76603126c3153f99769f3d191b62894f8a1c1a99
Gabriel Rosales <gabrielrosales@Gabriels-MacBook-Air.local>
2025-09-04 18:43:46 -0600

    feat/ adding searching

--- patient/views.py
+++ patient/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, Li
 from rest_framework.permissions import IsAuthenticated
 from patient.models import Patient
 from patient.serializers import PatientSerializer
+from rest_framework import filters
 
 class PatientCreateApiView(ListCreateAPIView):
     queryset = Patient.objects.all()
@@ -18,3 +19,6 @@ class PatientListApiView(ListAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields=["name"]
+
